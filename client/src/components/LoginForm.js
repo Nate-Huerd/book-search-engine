e74df9855a8157621b5122f6a630feae1b7d7f9c
Nate-Huerd <nhuerd3@gmail.com>
2022-05-16 12:40:48 -0500

    added chnage to LoginForm.js

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,9 +1,8 @@
 // see SignupForm.js for comments
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 // import mutations
-import { useMutation } from '@apollo/react-hooks';
-import { ADD_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
@@ -11,6 +10,14 @@ const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [login, { error }] = useMutation(loginUser);
+  useEffect(() => {
+    if (error) {
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+    }
+  }, [error]);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
